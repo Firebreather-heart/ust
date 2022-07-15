30eaab7cc1e7a9270722aef51aaae79a562082da
firebreather <dtenny95@gmail.com>
2022-07-16 00:41:03 +0100

    heroku ready

--- ustmain/stSite/stSite/settings.py
+++ ustmain/stSite/stSite/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-eydz+eoq3wa@80@don44y5dm&8++m415t$i@129i$9iii9&t6n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

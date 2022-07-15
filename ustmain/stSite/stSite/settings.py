95e2deab29c5068c3935468d12d7be2916fbc317
firebreather <dtenny95@gmail.com>
2022-07-16 00:44:57 +0100

    heroku done

--- ustmain/stSite/stSite/settings.py
+++ ustmain/stSite/stSite/settings.py
@@ -159,6 +159,9 @@ SOCIALACCOUNT_PROVIDERS = {
 STATIC_URL = 'static/'
 STATICFILES_DIRS =[os.path.join(BASE_DIR,'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field

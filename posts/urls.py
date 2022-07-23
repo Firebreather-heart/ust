90cd3df02c6deac3278dc87f9511bc97ec4a4f1c
firebreather <dtenny95@gmail.com>
2022-07-23 17:14:13 +0100

    all

--- posts/urls.py
+++ posts/urls.py
@@ -15,6 +15,6 @@ urlpatterns = [
     path('<uuid:pk>/delete/',ArticleDeleteView.as_view(),name='article_delete'),
     path('<uuid:pk>/edit/',ArticleUpdateView.as_view(),name='article_update'),
     path('search/',search_product,name='search'),
-    path('<uuid:pk>/detail',ArticleDetailView.as_view(),name='article_detail'),
+    path('<uuid:pk>/detail/',ArticleDetailView.as_view(),name='article_detail'),
     path('<uuid:pk>/comment/',commentView,name='comment'),
 ]

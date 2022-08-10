234dae068caec0e7d5d615634215ba4d33c330d1
firebreather <dtenny95@gmail.com>
2022-08-10 09:29:34 +0100

    glory

--- stSite/settings.py
+++ stSite/settings.py
@@ -29,7 +29,7 @@ if os.path.isfile(dotenv_file):dotenv.load_dotenv(dotenv_file)
 
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
